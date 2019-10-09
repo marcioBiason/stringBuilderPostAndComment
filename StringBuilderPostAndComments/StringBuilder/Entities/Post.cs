61c26a6e4003883769872807f9083db6b655d4af
marcBiason <54872072+marcioBiason@users.noreply.github.com>
2019-10-09 14:25:24 -0300

    Add files via upload

---
+++ StringBuilderPostAndComments/StringBuilder/Entities/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace StringBuilderPost.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        
+        //Metodo para adicionar um comentario
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        //Metodo para remover um comentario
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            //AppendLine Adiciona e pula a Linha
+            sb.AppendLine(Title);
+            //Append adiciona na mesma linha;
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+
+            //Laco de repeticao para adicionar todos os comentarios no Post;
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            //Retornando a string COnstruida;
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringBuilderPostAndComments/StringBuilder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using StringBuilderPost.Entities;
+
+namespace StringBuilderPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow thats awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zeland",
+                "I'm going to visit this wonderful country",
+                12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("Boa noite, parça");
+
+            Post p2 = new Post(
+                DateTime.Now,
+                "Good Night guys(us)/Boa noite parças(br)",
+                "See you tomorrow ninjas(us)/ Até amanhã rapaize(br)",
+                5);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
